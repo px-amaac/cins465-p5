211c630eff4d0bf5dda1886bd3ded5ba65f39442
DoubleAA <px.amaac@gmail.com>
2013-10-09 12:38:08 -0700

    commit to school

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,8 @@ class Address < ActiveRecord::Base
   before_save { |user| user.email = email.downcase }
   has_one :color
   has_one :zip_code
+  accepts_nested_attributes_for :color reject_if: :all_blank
+  accepts_nested_attributes_for :zip_code reject_if: :all_blank
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :firstName, :lastName , presence: true
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
